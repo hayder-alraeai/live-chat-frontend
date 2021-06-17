f39d687e33a53f2b821cb4850598f316d0cef133
hayder-alraeai <haydar860116@hotmail.com>
2021-06-17 11:28:57 +0200

    chat is working

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect} from 'react'
 import '../styles/Chat.css'
 import queryString from 'query-string'
 import io from 'socket.io-client'
-
+import ScrollToBottom from 'react-scroll-to-bottom'
 
 let socket;
 export default function Chat({ location }) {
@@ -38,15 +38,28 @@ export default function Chat({ location }) {
         if(message){
             socket.emit('sendMessage', message, () => {
                 setMessage('')
-            })  
+            }) 
+             
         }
     }
     console.log(message, messages);
     return (
         <div>
-            <input type='text' onChange={e => setMessage(e.target.value)}
+            {/* <div className='chat-display-wrapper'> */}
+                <ScrollToBottom>
+                {messages.map(m => {
+                    return(
+                        <div>
+                            <p>{m.user}:</p><span>{m.text}</span>
+                        </div>
+                    )
+                })}
+                </ScrollToBottom>
+            {/* </div> */}
+            <input type='text' value={message} onChange={e => setMessage(e.target.value)}
                                 onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null}
             />
+            <button onClick={(e) => sendMessage(e)}>Send</button>
         </div>
     )
 }
